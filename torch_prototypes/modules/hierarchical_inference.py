a820d642c4ed9f2513dfe523d00f8cab8993d1ae
VSainteuf <vivien.sainte-fare-garnot@polytechnique.edu>
2020-07-04 19:04:40 +0200

    no scatter

--- torch_prototypes/modules/hierarchical_inference.py
+++ torch_prototypes/modules/hierarchical_inference.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 
-from torch_scatter import scatter_softmax, scatter_sum, scatter_logsumexp
+#from torch_scatter import scatter_softmax, scatter_sum, scatter_logsumexp
+
 
 
 class HierarchicalInference(nn.Module):
