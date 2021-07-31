6aee141aaeb8ed1495435449c0abc265123c476e
VSainteuf <vivien.sainte-fare-garnot@polytechnique.edu>
2021-07-31 11:23:28 +0200

    missing import

--- torch_prototypes/modules/hierarchical_inference.py
+++ torch_prototypes/modules/hierarchical_inference.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-# from torch_scatter import scatter_softmax, scatter_sum, scatter_logsumexp
+from torch_scatter import scatter_softmax, scatter_sum, scatter_logsumexp
 
 
 class HierarchicalInference(nn.Module):
